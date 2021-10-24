ccccb6e6e97e58e295fb0993380463994c98b7bb
phantom9999 <1057369361@qq.com>
2021-10-24 08:16:53 +0000

    mv to session

--- jeprof_in_use/src/http_session.cpp
+++ jeprof_in_use/src/http_session.cpp
@@ -82,4 +82,65 @@ void HttpSession::do_close() {
     // At this point the connection is closed gracefully
 }
 
+void HttpSession::handle_request(http::request<http::string_body> &&req, HttpSession::queue& send) {
+    // AAA<decltype(send)> aa;
+    // Returns a bad request response
+    auto const bad_request =
+            [&req](beast::string_view why) {
+                http::response<http::string_body> res{http::status::bad_request, req.version()};
+                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                res.set(http::field::content_type, "text/html");
+                res.keep_alive(req.keep_alive());
+                res.body() = std::string(why);
+                res.prepare_payload();
+                return res;
+            };
+
+    // Returns a not found response
+    auto const not_found =
+            [&req](beast::string_view target) {
+                http::response<http::string_body> res{http::status::not_found, req.version()};
+                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                res.set(http::field::content_type, "text/html");
+                res.keep_alive(req.keep_alive());
+                res.body() = "The resource '" + std::string(target) + "' was not found.";
+                res.prepare_payload();
+                return res;
+            };
+
+    // Returns a server error response
+    auto const server_error =
+            [&req](beast::string_view what) {
+                http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                res.set(http::field::content_type, "text/html");
+                res.keep_alive(req.keep_alive());
+                res.body() = "An error occurred: '" + std::string(what) + "'";
+                res.prepare_payload();
+                return res;
+            };
+    auto const status_ok =
+            [&req](beast::string_view what) {
+                http::response<http::string_body> res{http::status::ok, req.version()};
+                res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+                res.set(http::field::content_type, "text/html");
+                res.keep_alive(req.keep_alive());
+                res.body() = std::string(what);
+                res.prepare_payload();
+                return res;
+            };
+
+    // Make sure we can handle the method
+    if (req.method() != http::verb::get && req.method() != http::verb::head) {
+        return send(bad_request("Unknown HTTP-method"));
+    }
+
+    // Request path must be absolute and not contain "..".
+    if (req.target().empty() || req.target()[0] != '/' || req.target().find("..") != beast::string_view::npos) {
+        return send(bad_request("Illegal request-target"));
+    }
+    std::cout << req.target() << std::endl;
+    send(status_ok("hello world"));
+}
+
 
