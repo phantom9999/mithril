2e579ec139ba3a11555e9d6ce686255fc3413a16
phantom9999 <1057369361@qq.com>
2021-12-25 07:25:50 +0000

    add hash mapo

---
+++ hashmap/src/segment_hash_map.cc
@@ -0,0 +1,5 @@
+//
+// Created by work on 12/25/21.
+//
+
+#include "segment_hash_map.h"
---
+++ hashmap/src/segment_hash_map.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <mutex>
+#include <absl/container/flat_hash_map.h>
+
+template<typename KeyType, typename ValueType>
+struct Segment {
+  absl::flat_hash_map<KeyType, ValueType> data;
+  std::mutex mutex;
+};
+
+/**
+ * 分段加锁的哈希表
+ */
+template<typename KeyType, typename ValueType, std::size_t SIZE = 10>
+class SegmentHashMap {
+ public:
+  SegmentHashMap() = default;
+
+  bool get(KeyType key, ValueType* value) {
+    size_t index = hash_func_(key) % SIZE;
+    auto& buffer = segment_[index];
+    std::lock_guard guard{buffer.mutex};
+    auto it = buffer.data.find(key);
+    if (it == buffer.data.end()) {
+      return false;
+    }
+    *value = it->second;
+    return true;
+  }
+
+  void set(KeyType key, KeyType value) {
+    size_t index = hash_func_(key) % SIZE;
+    auto& buffer = segment_[index];
+    std::lock_guard guard{buffer.mutex};
+    buffer.data.emplace(key, value);
+  }
+
+ private:
+  Segment<KeyType, ValueType> segment_[SIZE];
+  std::hash<int32_t> hash_func_;
+};
