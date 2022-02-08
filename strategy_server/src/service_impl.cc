11ac4bb50dc22f93a0e3c3641be2b68b2437db52
phantom9999 <1057369361@qq.com>
2022-02-08 07:19:03 +0000

    添加动态

--- strategy_server/src/service_impl.cc
+++ strategy_server/src/service_impl.cc
@@ -5,21 +5,22 @@
 #include "strategies/strategy_b.h"
 #include "strategies/strategy_c.h"
 #include "strategies/strategy_d.h"
+#include <unordered_map>
 
 ::grpc::Status ServiceImpl::Rank(::grpc::ServerContext *context, const ::StrategyRequest *request,
                     ::StrategyResponse *response) {
   tf::Taskflow taskflow;
   auto session = std::make_shared<Session>();
   {
-    auto [A, B, C, D] = taskflow.emplace(  // create four tasks
-        createStrategy<StrategyA>(session),
-        createStrategy<StrategyB>(session),
-        createStrategy<StrategyC>(session),
-        createStrategy<StrategyD>(session)
-    );
-
-    A.precede(B, C);  // A runs before B and C
-    D.succeed(B, C);  // D runs after  B and C
+    std::unordered_map<std::string, tf::Task> taskmap;
+    taskmap.insert({"StrategyA", taskflow.emplace(createStrategy<StrategyA>(session))});
+    taskmap.insert({"StrategyB", taskflow.emplace(createStrategy<StrategyB>(session))});
+    taskmap.insert({"StrategyC", taskflow.emplace(createStrategy<StrategyC>(session))});
+    taskmap.insert({"StrategyD", taskflow.emplace(createStrategy<StrategyD>(session))});
+    taskmap["StrategyA"].precede(taskmap["StrategyB"]);
+    taskmap["StrategyA"].precede(taskmap["StrategyC"]);
+    taskmap["StrategyD"].succeed(taskmap["StrategyB"]);
+    taskmap["StrategyD"].succeed(taskmap["StrategyC"]);
   }
 
   session->set(SessionType::REQUEST, request);
