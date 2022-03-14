e84331c40ab93444fe721b78737da6ca21ecb2d2
phantom9999 <1057369361@qq.com>
2022-03-14 22:21:52 +0800

    add python3

--- docker_dev/vcpkg_env/install.sh
+++ docker_dev/vcpkg_env/install.sh
@@ -3,7 +3,9 @@
 set -x
 
 source /opt/dev.sh;
-vcpkg install --clean-after-build gflags glog folly grpc boost fmt;
+vcpkg install --clean-after-build gflags glog folly grpc boost rocksdb;
+cd ~;
+rm -rf .cache/ || true
 #cd /opt/vcpkg_home;
 #rm -rf buildtrees/*
 #rm -f downloads/*.gz
