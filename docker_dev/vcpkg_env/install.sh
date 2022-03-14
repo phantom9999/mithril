8ca312c993d568754d99ebd5520245a58beafe85
phantom9999 <phantom9999@users.noreply.github.com>
2022-03-14 20:40:07 +0800

    Update install.sh

--- docker_dev/vcpkg_env/install.sh
+++ docker_dev/vcpkg_env/install.sh
@@ -3,7 +3,7 @@
 set -x
 
 source /opt/dev.sh;
-vcpkg install gflags glog folly grpc boost fmt;
-cd /opt/vcpkg_home;
-rm -rf buildtrees/*
-rm -f downloads/*.gz
+vcpkg install --clean-after-build gflags glog folly grpc boost fmt;
+#cd /opt/vcpkg_home;
+#rm -rf buildtrees/*
+#rm -f downloads/*.gz
