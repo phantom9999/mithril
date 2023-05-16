b9f30e0d0456161cff5e5f999349bcb778f0c391
phantom9999 <1057369361@qq.com>
2023-05-16 23:49:20 +0800

    add cu

--- cuda_learning/main.cu
+++ cuda_learning/main.cu
@@ -4,17 +4,23 @@
 
 int main() {
     int count = 0;
-    cudaGetDeviceCount(&count);
-    std::cout << "gpu count: " << count << std::endl;
     cudaError_t err = cudaGetDeviceCount(&count);
     if (err != cudaSuccess) {
         std::cout << cudaGetErrorString(err) << std::endl;
+        return 0;
     }
-    cudaDeviceProp prop;
-    cudaGetDeviceProperties(&prop, 0);
-    std::cout << "device number: " << 0 << std::endl;
-    std::cout << "name: " << prop.name << std::endl;
-    std::cout << "memory: " << prop.totalGlobalMem << std::endl;
-    std::cout << "threads per block: " << prop.maxThreadsPerBlock << std::endl;
+    std::cout << "gpu count: " << count << std::endl;
+    for (int i=0; i<count; ++i) {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, i);
+        std::cout << "device number: " << 0 << std::endl;
+        std::cout << "name: " << prop.name << std::endl;
+        std::cout << "memory: " << prop.totalGlobalMem << std::endl;
+        std::cout << "threads per block: " << prop.maxThreadsPerBlock << std::endl;
+        std::cout << "compute: " << prop.major << "." << prop.minor << std::endl;
+        std::cout << "ms count: " << prop.multiProcessorCount << std::endl;
+        std::cout << "thread wrap size: " << prop.warpSize << std::endl;
+    }
+
   return 0;
 }
