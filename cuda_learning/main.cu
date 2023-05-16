aae210c459c9155e2dc0ed19acd78d4a889727ed
phantom9999 <1057369361@qq.com>
2023-05-16 23:43:20 +0800

    add cu

--- cuda_learning/main.cu
+++ cuda_learning/main.cu
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
+#include <cuda_runtime.h>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+    int count = 0;
+    cudaGetDeviceCount(&count);
+    std::cout << "gpu count: " << count << std::endl;
+    cudaError_t err = cudaGetDeviceCount(&count);
+    if (err != cudaSuccess) {
+        std::cout << cudaGetErrorString(err) << std::endl;
+    }
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, 0);
+    std::cout << "device number: " << 0 << std::endl;
+    std::cout << "name: " << prop.name << std::endl;
+    std::cout << "memory: " << prop.totalGlobalMem << std::endl;
+    std::cout << "threads per block: " << prop.maxThreadsPerBlock << std::endl;
   return 0;
 }
